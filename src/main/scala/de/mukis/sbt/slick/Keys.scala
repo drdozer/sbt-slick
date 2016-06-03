679ee185a720e4d30e074336e15bce7551b351e5
Matthew Pocock <turingatemyhamster@gmail.com>
2016-06-03 21:28:52 +0100

    Converted some settings into tasks so that they can be re-computed dynamically.

--- src/main/scala/de/mukis/sbt/slick/Keys.scala
+++ src/main/scala/de/mukis/sbt/slick/Keys.scala
@@ -20,13 +20,13 @@ trait Keys {
 
   val slickDriver = SettingKey[String]("slick-driver", "The slick driver, e.g. 'scala.slick.driver.MySQLDriver'")
   val slickJDBCDriver = SettingKey[String]("slick-jdbc-driver", "The jdbc driver, e.g. 'com.mysql.jdbc.Driver'")
-  val slickUrl = SettingKey[MakeUrl]("slick-url", "URL to database, e.g. 'jdbc:mysql://localhost:3306/your-db'")
-  val slickArguments = SettingKey[MakeArguments]("slick-arguments", "Arguments array for database")
+  val slickUrl = TaskKey[MakeUrl]("slick-url", "URL to database, e.g. 'jdbc:mysql://localhost:3306/your-db'")
+  val slickArguments = TaskKey[MakeArguments]("slick-arguments", "Arguments array for database")
   val slickUser = SettingKey[Option[String]]("slick-user", "User to access database")
   val slickPassword = SettingKey[Option[String]]("slick-password", "Password to access database")
-  val slickPort = SettingKey[Int]("slick-port", "Port to access database")
+  val slickPort = TaskKey[Int]("slick-port", "Port to access database")
   val slickDatabases = SettingKey[Seq[String]]("slick-databases", "databases to generate tables for")
   val slickPackage = SettingKey[String]("slick-package", "Output package for Tables.scala")
-  val slickMakeDbPackage = SettingKey[MakePackage]("slick-db-package", "Call for each database to generate the package name")
+  val slickMakeDbPackage = TaskKey[MakePackage]("slick-db-package", "Call for each database to generate the package name")
   val slickGenTables = TaskKey[Seq[File]]("slick-gen-tables", "Task to generate the table")
 }
